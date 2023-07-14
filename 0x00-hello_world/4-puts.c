8268746fdf38ac926a81f4085f60512e4c30e7df
Said JAOUAD <jaouad_sd@hotmail.com>
2023-07-13 18:00:19 -0700

    Hello Again

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * return : 0 => Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
