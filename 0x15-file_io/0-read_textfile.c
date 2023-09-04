baec4c69b8cc35be719408503509869b7fecc086
jaouad-said <jaouad_sd@hotmail.com>
2023-09-04 23:37:07 +0100

    Modify Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,26 +7,17 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int a, b, c;
-	char *chr;
+	int ab;
+	ssize_t bts;
+	char buffer[GET_BUFFER_SIZE * 8];
 
-	a = open(filename, O_RDONLY);
-	if (f == -1)
+	if (!filename || !letters)
 		return (0);
-	chr = malloc(sizeof(chr) * letters);
-	if (chr == NULL)
-	{
-		clode(a);
+	ab = open(filename, O_RDONLY);
+	if (ab == -1)
 		return (0);
-	}
-	b = read(a, chr, letters);
-	c = write(STDOUT_FILENO, chr, b);
-	if (b == -1)
-	{
-		free(chr);
-		close(a);
-		return (0);
-	}
-	close(a);
-	return (c);
+	bts = read(ab, &buffer[0], letters);
+	bts = write(STDOUT_FILENO, &buffer[0], bts);
+	close(ab);
+	return (bts);
 }
