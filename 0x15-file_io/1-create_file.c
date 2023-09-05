deac18dc5ff1f0cd93a729d70118bd20f7baf517
Jaouad-Said <73340486+Jaouad-Said@users.noreply.github.com>
2023-09-05 12:54:39 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * create_file - creates the file
- *  @filename: file that should be created
- *  @text_content: content of that file
- *  Return: int
+ * _strlen - returns length of a string
+ * @s: the string
+ *
+ * Return: integer length of string
  */
 int _strlen(char *s)
 {
@@ -17,6 +17,12 @@ int _strlen(char *s)
 	return (i);
 }
 
+/**
+ *  * create_file - creates the file
+ *  @filename: file that should be created
+ *  @text_content: content of that file
+ *  Return: int
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fol;
