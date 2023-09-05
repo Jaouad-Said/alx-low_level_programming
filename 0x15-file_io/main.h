e76b21acf86b86f06cb107f0cc288e5fd26bd000
Jaouad-Said <73340486+Jaouad-Said@users.noreply.github.com>
2023-09-05 12:50:09 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-#define GET_BUFFER_Size 1024
+#define READ_BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
