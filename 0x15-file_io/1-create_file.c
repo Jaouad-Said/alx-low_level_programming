ac25d4ee3b3edeaf467838b1f3928d20e7577837
Jaouad-Said <73340486+Jaouad-Said@users.noreply.github.com>
2023-09-05 12:52:43 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,22 +5,30 @@
  *  @text_content: content of that file
  *  Return: int
  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
 int create_file(const char *filename, char *text_content)
 {
-	int f, len = 0, w;
+	int fol;
+	ssize_t bts = 0, len = _strlen(text_content);
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	while (text_content != NULL && text_content[len])
-	{
-		len++;
-	}
-	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (w == -1)
+	fol = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fol == -1)
 		return (-1);
-	w = write(f, text_content, len);
-	if (w == -1)
-		return (-1);
-	close(f);
-	return (1);
+	if (len)
+		bts = write(fol, text_content, len);
+	close(fol);
+	return (bts == len ? 1 : -1);
 }
