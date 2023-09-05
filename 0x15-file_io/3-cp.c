8ba4b45e907c0a1abd1c96a6694070042860a29b
Jaouad-Said <73340486+Jaouad-Said@users.noreply.github.com>
2023-09-05 13:57:25 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,31 +15,31 @@
  */
 int main(int ac, char **av)
 {
-	int from_fol = 0, to_fd = 0;
+	int from_fol = 0, to_fol = 0;
 	ssize_t b;
-	char buffer[READ_BUF_SIZE];
+	char buffer[READ_BUFFER_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fd = open(av[1], O_RDONLY);
-	if (from_fd == -1)
+	from_fol = open(av[1], O_RDONLY);
+	if (from_fol == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_fd == -1)
+	to_fol = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fol == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
-		if (write(to_fd, buf, b) != b)
+	while ((b = read(from_fol, buf, READ_BUFFER_SIZE)) > 0)
+		if (write(to_fol, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	from_fd = close(from_fd);
-	to_fd = close(to_fd);
-	if (from_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
-	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	from_fol = close(from_fol);
+	to_fol = close(to_fol);
+	if (from_fol)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fol), exit(100);
+	if (to_fol)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fol), exit(100);
 
 	return (EXIT_SUCCESS);
 }
